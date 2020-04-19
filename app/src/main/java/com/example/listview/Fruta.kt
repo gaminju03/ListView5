e8d07c08a5194093213af7864edd9f6f0f3fa12f
Juan Gamino <gamino_juan03@hotmail.com>
2020-04-19 18:07:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/listview/Fruta.kt
@@ -0,0 +1,9 @@
+package com.example.listview
+
+class Fruta (nombre:String, Imagen:Int) {
+    var nombre:String =""
+    var Imagen:Int = 0
+    init{
+        this.nombre = nombre
+        this.Imagen = Imagen
+    } }
